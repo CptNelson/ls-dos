a377cdbda0da6a33ec31f9228a756f50d7fd4bb9
CptNelson <toni.lappalainen@gmail.com>
2021-11-05 15:12:36 +0200

    small fix

--- src/js/command.js
+++ src/js/command.js
@@ -1,19 +1,32 @@
 const directories = ['music', 'games'];
 
-const dir = () => {
-	const string = `
+const defaultStringStart = `
     <br/>
-     Volume in drive C is LS-DOS4 <br/>
-     Volume serial number is 93E2-7A23 <br/>
-     Directory of C:\\ <br/>
+    \u00A0 Volume in drive C is LS-DOS4 <br/>
+    \u00A0 Volume serial number is 93E2-7A23 <br/>
+    \u00A0 Directory of C:\\ <br/>
      <br/>
-    ${directories.map((dir) => `${dir.toUpperCase()} \u00A0 \u00A0\u00A0\u00A0 &lt;DIR&gt; \u00A0\u00A0 23-09-94 \u00A023:23a <br/>`).join("")}
-    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 ${directories.length} file(s) \u00A0\u00A0\u00A0 42069 bytes <br/>
+
+`;
+
+const defaultStringEnd = `
+\u00A0\u00A0\u00A0 42069 bytes <br/>
     \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A09360512 bytes free
 
     <br/>
-    `;
-	return string;
+
+`;
+
+const dir = () => {
+	const string = `
+    ${directories
+		.map(
+			(dir) =>
+				`${dir.toUpperCase()} \u00A0 \u00A0\u00A0\u00A0 &lt;DIR&gt; \u00A0\u00A0 23-09-94 \u00A023:23a <br/>`
+		)
+		.join('')}
+    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 ${directories.length} file(s)     `;
+	return defaultStringStart + string + defaultStringEnd;
 };
 
 const command = (cmd) => {
---
+++ src/js/directory.js
@@ -0,0 +1,29 @@
+export default dir = [
+	{
+		name: 'MUSIC',
+		content: [
+			{
+				name: 'CREATE',
+				content: [
+					{
+						name: 'SCRIBBLE',
+						url: 'http://scribble.audio/',
+						desc: 'Make music by drawing shapes.',
+					},
+					{
+						name: 'TYPEDRUMMER',
+						url: 'http://typedrummer.com/',
+						desc: 'Make music by typing text.',
+					},
+					{
+						name: '808303',
+						url: 'https://808303.studio/',
+						desc: 'Make music with 808 and 303.',
+					},
+				],
+				name: 'LISTEN',
+				content: {},
+			},
+		],
+	},
+];
